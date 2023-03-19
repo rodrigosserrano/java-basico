c32d771ed262dba287a5844aa82377872159024a
Rodrigo Serrano <rodrigooh.dl@gmail.com>
2023-03-19 19:18:20 -0300

    Operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,13 @@
+public class Operadores {
+    public static void main(String[] args) {
+        String concatenacao = "?";
+
+        concatenacao = 1+1+1+"1";
+        concatenacao = 1+"1"+1+1;
+        concatenacao = 1+"1"+1+"1";
+        concatenacao = "1"+1+1+1;
+        concatenacao = "1"+(1+1+1);
+
+        System.out.println(concatenacao);
+    }
+}
