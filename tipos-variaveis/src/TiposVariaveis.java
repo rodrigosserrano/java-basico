ef508f2ebb435be9f0cdae9a22de60013bc816e2
Rodrigo Serrano <rodrigooh.dl@gmail.com>
2023-03-19 16:56:51 -0300

    Casting em Java, setando float e long em java // constante em java

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,21 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello World!");
+        // Definindo um double
+        double salarioMinimo = 2500.50;
+
+        // Definindo um float, é necessário adicionar um F no final para que o java não faça a conversão para double
+        float salarioMinimo2 = 2500.50F;
+
+        // Definir um long porecisa setar um L no final para que o Java nao converta para int
+        long numero2 = 100L;
+
+        // inteiro
+        int inteiro = 2;
+
+        // Fazer um casting de uma variável
+        short inteiroCasting = (short) inteiro;
+        
+        // Constante em java | Deve adicionar 'final', por convenção a varíavel deve ser escrito em maiúsculo
+        final int NUMERO = 245;
     }
 }
