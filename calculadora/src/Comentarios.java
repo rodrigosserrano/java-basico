c200b94a6ff5d2cb8764a7f724bc4ece4e2096e3
Rodrigo Serrano <rodrigooh.dl@gmail.com>
2023-03-19 22:50:31 -0300

    Javadoc

---
+++ calculadora/src/Comentarios.java
@@ -0,0 +1,30 @@
+public class Comentarios {
+    public static void main(String[] args) {
+        // Olá, eu sou um comentário
+
+        /*
+        * bloco de comentário
+         */
+
+        /**
+         * Documentação
+         */
+    }
+
+    /*
+     * Este método foi elaborado as pressas
+     * por isso eu abrevei o nome das variáveis
+     * mas olha, ele tem a finalidade de somar ou  multiplicar
+     * dois números
+     */
+    public int somaMultiplica (int n, int x, String m){
+        int r = 0; // r é igual ao resultado
+        if (m == "M"){ // M= multiplicação
+            r= n * x;
+        } else {
+            // se não soma mesmo
+            r = n + x;
+        }
+        return r;
+    }
+}
