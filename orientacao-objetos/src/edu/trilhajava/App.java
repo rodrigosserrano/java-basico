eb02a40c056d65837b6a2c72b6e4b6c8f617cd52
Rodrigo Serrano <rodrigooh.dl@gmail.com>
2023-04-05 00:00:36 -0300

    inicio orientação a objetos em java

---
+++ orientacao-objetos/src/edu/trilhajava/App.java
@@ -0,0 +1,9 @@
+package edu.trilhajava;
+
+public class App {
+    public static void main(String[] args) {
+        Carro ford = new Carro("Branco", "Ford", 50);
+
+        System.out.println(ford.print());
+    }
+}
